0c192c4034c13375d590d815fec837ab7104adb6
AVS <123315354+AVS-Memer@users.noreply.github.com>
2026-03-08 14:32:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,29 @@ async function loadProblems() {
     const problems = await res.json(); // this should be an array of {id, title, tags}
     const fragment = document.createDocumentFragment();
 
-    problems.forEach(({ id, title, tags }) => {
-      const li = document.createElement("li");
+    problems.forEach(({ id, title, tags, difficulty }) => {
+      const tr = document.createElement("tr");
+
+      const idTd = document.createElement("td");
+      idTd.textContent = id;
+      tr.appendChild(idTd);
+
+      const titleTd = document.createElement("td");
       const a = document.createElement("a");
-
-      a.textContent = `#${id}: ${title}`;
       a.href = `./problems?problem=${id}`;
+      a.textContent = title;
+      titleTd.appendChild(a);
+      tr.appendChild(titleTd);
 
-      li.appendChild(a);
-      li.append(` [${tags.join(", ")}]`);
-      fragment.appendChild(li);
+      const tagsTd = document.createElement("td");
+      tagsTd.textContent = tags.join(", ");
+      tr.appendChild(tagsTd);
+
+      const diffTd = document.createElement("td");
+      diffTd.textContent = difficulty !== undefined ? difficulty : "-";
+      tr.appendChild(diffTd);
+
+      fragment.appendChild(tr);
     });
 
     problemList.appendChild(fragment);
